34f3eabee38437764f4a1ba15c58f9f5783e46c1
vnach291 <vnach291@gmail.com>
2023-09-15 16:16:04 -0500

    worked on spacing buttons and a bunch of other stuff :O

---
+++ Models/TitleMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMobileApp.Models
+{
+    public static internal class TitleMain
+    {
+        public static String mainTitle { get; } = "Main Page View";
+
+
+           
+    }
+}
