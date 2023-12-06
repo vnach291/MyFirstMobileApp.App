45b31228d72cf99e635ab66333e947253129edce
vnach291 <vnach291@gmail.com>
2023-12-06 13:04:40 -0600

    done switch stuff

--- ViewViewModels/Controls/StepperControls/StepperControlsView.xaml.cs
+++ ViewViewModels/Controls/StepperControls/StepperControlsView.xaml.cs
@@ -2,8 +2,20 @@ namespace MyFirstMobileApp.ViewViewModels.Controls.StepperControls;
 
 public partial class StepperControlsView : ContentPage
 {
-	public StepperControlsView()
-	{
-		InitializeComponent();
-	}
+    public StepperControlsView()
+    {
+        InitializeComponent();
+        BindingContext = new StepperControlsViewModel();
+    }
+
+    public void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        double value = e.NewValue;
+        RotatingText.Rotation = value;
+        int R = (int)(value / 360 * 255);
+        int G = (int)(value / 360 * 184);
+        int B = (int)(value / 360 * 200);
+        DisplayLabel.TextColor = Color.FromRgb(R, G, B);
+        DisplayLabel.Text = string.Format("The Stepper value is {0}", value);
+    }
 }
