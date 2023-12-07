7d62be07c5c1cf5cc0ba31246d324e4b330b6a4e
vnach291 <vnach291@gmail.com>
2023-12-07 10:58:49 -0600

    switch done too but need to fix titles and some other stuff

--- ViewViewModels/Controls/SwitchControls/SwitchControlsView.xaml.cs
+++ ViewViewModels/Controls/SwitchControls/SwitchControlsView.xaml.cs
@@ -5,5 +5,30 @@ public partial class SwitchControlsView : ContentPage
 	public SwitchControlsView()
 	{
 		InitializeComponent();
+        BindingContext = new SwitchControlsViewModel();
 	}
+
+    private void Switch1Toggled(object sender, ToggledEventArgs e)
+    {
+        if (e.Value == false)
+        {
+            label1.TextColor = Color.FromArgb("#000000");
+        }
+        else
+        {
+            label1.TextColor = Color.FromArgb("#ffb8c8");
+        }
+    }
+
+    private void Switch2Toggled(object sender, ToggledEventArgs e)
+    {
+        if (e.Value == false)
+        {
+            label2.TextColor = Color.FromArgb("000000");
+        }
+        else
+        {
+            label2.TextColor = Color.FromArgb("#03fcd3");
+        }
+    }
 }
--- ViewViewModels/Controls/SwitchControls/SwitchControlsViewModel.cs
+++ ViewViewModels/Controls/SwitchControls/SwitchControlsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstMobileApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace MyFirstMobileApp.ViewViewModels.Controls.SwitchControls
 {
-    internal class SwitchControlsViewModel
+    public class SwitchControlsViewModel : BaseViewModel
     {
+        public bool Button1Initial { get; set; } = true;
+        public bool Button2Initial { get; set; } = false;
+    
+        public SwitchControlsViewModel()
+        {
+            //
+        }
+
+
     }
+
 }
